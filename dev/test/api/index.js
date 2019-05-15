2f28531f26fb726d87657699ff01070d89bee781
Jon McClure <jmcclure@politico.com>
2019-05-14 22:05:12 -0400

    testing

--- dev/test/api/index.js
+++ dev/test/api/index.js
@@ -1,6 +1,8 @@
+import fs from 'fs';
 import expect from 'expect.js';
 import portfinder from 'portfinder';
 import fetch from 'node-fetch';
+import cheerio from 'cheerio';
 import expressServer from 'Bakery/server/';
 import { sweepTmp } from 'Bakery/utils/';
 
@@ -11,6 +13,7 @@ const TOKEN = 'Token TESTING';
 describe('tests baking', async function() {
   before(async function() {
     process.env.API_VERIFICATION_TOKEN = TOKEN;
+    process.env.TESTING = true;
     portfinder.basePort = 3333;
     port = await portfinder.getPortPromise().then(p => p);
     server = await expressServer.listen(port, () => console.log(`Bakery server listening on port ${port}!`));
@@ -33,7 +36,46 @@ describe('tests baking', async function() {
     expect(response.status).to.be(200);
   });
 
+  it('tests the simple bake', async function() {
+    const response = await fetch(`http://localhost:${port}`, {
+      headers: {
+        Authorization: `Token ${TOKEN}`,
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        action: 'bake_simple',
+        data: [],
+      }),
+    })
+      .then(response => response);
+    expect(response.status).to.be(200);
+
+    const $ = cheerio.load(fs.readFileSync('.tmp/bakery/simple-page-test/index.html'));
+    expect($('h1').text()).to.be('hello world');
+  });
+
+  it('tests the home bake', async function() {
+    const response = await fetch(`http://localhost:${port}`, {
+      headers: {
+        Authorization: `Token ${TOKEN}`,
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        action: 'bake_home',
+        data: { name: 'POLITICO Interactives' },
+      }),
+    })
+      .then(response => response);
+    expect(response.status).to.be(200);
+
+    const $ = cheerio.load(fs.readFileSync('.tmp/bakery/index.html'));
+    expect($('h1').text()).to.be('Hello from POLITICO Interactives');
+  });
+
   after(function() {
     server.close();
+    sweepTmp();
   });
 });
